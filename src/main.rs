5be86b3a64fc7899f2cab4164f9313fe341e9e42
MiguelSiTho <miguel7stv@gmail.com>
2022-11-15 15:45:52 -0300

    upgrade match

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 fn main() {
 
-    let x = 27;
-    let y = 7;
-    let w = 1;
+    let x: f64 = 10.5;
+    let y: f64 = 7.3;
+    let w = 4;
         
     match w{
-        1=> println!("{}", x + y),
-        2=> println!("{}", x - y),
-        3=> println!("{}", x * y),
-        4=> println!("{}", x / y),
-        5=> println!("{}", x ^ y),  
-        6=> println!("{}", x % y),
-        _=> println!("asd"),
+        1=> println!("{} + {} = {}", x , y,  x + y),
+        2=> println!("{} - {} = {}", x , y,  x - y),
+        3=> println!("{} * {} = {}", x , y,  x * y),
+        4=> println!("{} / {} = {}", x , y,  x / y),
+        5=> println!("{} ^ {} = {}", x , y,  f64::powf(x, y)),  
+        6=> println!("The rest of {} / {} is {}",x , y,  x % y),
+        _=> println!("error in data"),
     }
  
 }
