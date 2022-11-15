1d131132e3ac580ee037a870ca1b0cc9617fac9b
MiguelSiTho <miguel7stv@gmail.com>
2022-11-14 21:48:42 -0300

    created calculator files

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let x = 5;
+    let y = 6;
+    let w = 2;
+
+    if w == 1 {
+        let z = x + y;
+        println!("{}", z);
+    } else if w == 2 {
+        let z = x - y;
+        println!("{}", z);
+    } else if w == 3 {
+        let z = x * y;
+        println!("{}", z);
+    } else if w == 4 {
+        let z = x / y;
+        println!("{}", z);
+    } else if w == 5 {
+        let z = x % y;
+        println!("{}", z);
+    } else if w == 6 {
+        let z = x ^ y;
+        println!("{}", z);
+    } 
+
+
+    
+}
