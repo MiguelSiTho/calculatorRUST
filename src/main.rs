a5d2514a085b812ef77f78af51180bc823b6215e
MiguelSiTho <miguel7stv@gmail.com>
2022-11-15 16:56:16 -0300

    adding inputs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
-fn main() {
+use std::io;
 
-    let x: f64 = 10.5;
-    let y: f64 = 7.3;
-    let w = 4;
-        
+fn main() {
+    println!("enter a number for x:");
+    let mut input1 = String::new();
+    io::stdin()
+           .read_line(&mut input1)
+           .expect("Failed to read input");
+
+    println!("enter a number for y:");
+    let mut input2 = String::new();
+    io::stdin()
+          .read_line(&mut input2)
+          .expect("Failed to read input");
+
+    println!("Choose a option: 1[addition], 2[subtraction], 3[multiplication], 4[division], 5[exponatiation], 6[The rest of x / y]");
+    let mut input3 = String::new();
+    io::stdin()
+         .read_line(&mut input3)
+         .expect("Failed to read input");
+         
+    let x: f64 = input1.trim().parse().expect("error");
+    let y: f64 = input2.trim().parse().expect("error");
+    let w: i32 = input3.trim().parse().expect("error");
+    
     match w{
         1=> println!("{} + {} = {}", x , y,  x + y),
         2=> println!("{} - {} = {}", x , y,  x - y),
