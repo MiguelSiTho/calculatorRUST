2400d3c9c189da71eac7e7700151bc537d24eb08
MiguelSiTho <miguel7stv@gmail.com>
2022-11-15 14:25:02 -0300

    replacing the ifs by match

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,18 @@
 fn main() {
-    let x = 5;
-    let y = 6;
-    let w = 2;
 
-    if w == 1 {
-        let z = x + y;
-        println!("{}", z);
-    } else if w == 2 {
-        let z = x - y;
-        println!("{}", z);
-    } else if w == 3 {
-        let z = x * y;
-        println!("{}", z);
-    } else if w == 4 {
-        let z = x / y;
-        println!("{}", z);
-    } else if w == 5 {
-        let z = x % y;
-        println!("{}", z);
-    } else if w == 6 {
-        let z = x ^ y;
-        println!("{}", z);
-    } 
-
-
-    
+    let x = 27;
+    let y = 7;
+    let w = 1;
+        
+    match w{
+        1=> println!("{}", x + y),
+        2=> println!("{}", x - y),
+        3=> println!("{}", x * y),
+        4=> println!("{}", x / y),
+        5=> println!("{}", x ^ y),  
+        6=> println!("{}", x % y),
+        _=> println!("asd"),
+    }
+ 
 }
+
